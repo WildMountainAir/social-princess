a2862066981ab068060fb4795d795224405225a9
Alison Sipos <alison.sipos@icloud.com>
2020-08-06 14:01:37 -0600

    Created data structures for message stream including users and home.

---
+++ data_generator.js
@@ -0,0 +1,24 @@
+/*
+NOTE: This file generates data used to create fake messages.
+*/
+
+// Data Structures //
+window.streams = {};
+streams.home = [];
+streams.users = {};
+strams.users.anna = [];
+streams.users.ariel = [];
+streams.users.aurora = [];
+streams.users.belle = [];
+streams.users.cinderella = [];
+streams.users.elsa = [];
+streams.users.famulan = [];
+streams.users.jasmin = [];
+streams.users.merida = [];
+streams.users.moana = [];
+streams.users.pocahontas = [];
+streams.users.rapunzel = [];
+streams.users.snowwhite = [];
+streams.users.sofia = [];
+streams.users.tiana = [];
+window.users = Object.keys(stream.users);
